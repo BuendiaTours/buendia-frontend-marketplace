761a20905fd0af6f00bee9337824173af5d80b95
Carlos Cabo <carloscabo@buendia.travel>
2026-01-26 15:56:02 +0100

    Modifying attractions data parse to include  key

--- src/lib/api/endpoints/attractions.ts
+++ src/lib/api/endpoints/attractions.ts
@@ -6,20 +6,20 @@ export const attractionsEndpoints = {
 	async getAll(fetchFn: typeof fetch): Promise<Attraction[]> {
 		const path = API_ENDPOINTS.attractions.list();
 
-		const response = await apiClient.request<Attraction[]>(fetchFn, path, {
+		const response = await apiClient.request<{ data: Attraction[] }>(fetchFn, path, {
 			method: 'GET'
 		});
 
-		return response.data;
+		return response.data.data;
 	},
 
 	async getById(fetchFn: typeof fetch, id: string): Promise<Attraction> {
 		const path = API_ENDPOINTS.attractions.detail(id);
 
-		const response = await apiClient.request<Attraction>(fetchFn, path, {
+		const response = await apiClient.request<{ data: Attraction }>(fetchFn, path, {
 			method: 'GET'
 		});
 
-		return response.data;
+		return response.data.data;
 	}
 };
