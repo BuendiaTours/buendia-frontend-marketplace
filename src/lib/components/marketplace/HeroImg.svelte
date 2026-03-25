c3ae8eace500290d0af16b517aceed75842c4893
Carlos Cabo <carloscabo@buendia.travel>
2026-03-25 12:06:29 +0100

    feat: image hero with srcSet

--- src/lib/components/marketplace/HeroImg.svelte
+++ src/lib/components/marketplace/HeroImg.svelte
@@ -12,11 +12,19 @@
 
 {#if imgObj?.originalUrl}
 	<div class="c-hero-img relative h-[196px] overflow-hidden rounded-xl sm:h-[314px] {wrapperClass}">
-		<img
-			class="h-full w-full object-cover object-center"
-			src={imgObj.variants?.find((v) => v.preset === 'HERO')?.url ?? imgObj.originalUrl}
-			alt={imgObj.altText ?? ''}
-		/>
+		<picture>
+			{#if imgObj.variants?.find((v) => v.preset === '16_9')?.url}
+				<source
+					media="(max-width: 600px)"
+					srcset={imgObj.variants?.find((v) => v.preset === '16_9')?.url}
+				/>
+			{/if}
+			<img
+				class="h-full w-full object-cover object-center"
+				src={imgObj.variants?.find((v) => v.preset === 'HERO')?.url ?? imgObj.originalUrl}
+				alt={imgObj.altText ?? ''}
+			/>
+		</picture>
 		<div class="bg-alpha-ink-48 absolute inset-0"></div>
 		{#if title}
 			<div class="absolute inset-0 flex items-center justify-center">
