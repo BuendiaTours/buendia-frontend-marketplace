c2e051d9e738c7937194e91562a2a90d6f4394aa
Carlos Cabo <carloscabo@buendia.travel>
2026-04-16 08:35:31 +0200

    feat: checkout base views

---
+++ src/routes/(checkout)/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../(marketplace)/layout-marketplace.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+</svelte:head>
+
+<div class="flex min-h-screen flex-col">
+	<main class="layout-content-area flex-1">
+		{@render children()}
+	</main>
+</div>
---
+++ src/routes/(checkout)/checkout-thank-you/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Checkout Thank You</h1>
