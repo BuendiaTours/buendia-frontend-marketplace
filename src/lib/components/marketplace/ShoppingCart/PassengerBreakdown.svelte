86f5dcb38b2b292321cd3128cb909039ceeebd7b
Carlos Cabo <carloscabo@buendia.travel>
2026-04-23 10:12:35 +0200

    refactor: add passenger breakdown

--- src/lib/components/marketplace/ShoppingCart/PassengerBreakdown.svelte
+++ src/lib/components/marketplace/ShoppingCart/PassengerBreakdown.svelte
@@ -5,22 +5,24 @@
 
 	type Props = {
 		items: PassengerLineItem[];
-		class?: string;
-		itemClass?: string;
+		wrapperClass?: string;
 	};
 
-	let { items, class: className = '', itemClass = '' }: Props = $props();
+	let { items, wrapperClass }: Props = $props();
 
 	const messages = m as unknown as Record<string, () => string>;
 </script>
 
-<ul class={className}>
-	{#each items.filter((i) => i.count > 0) as item (item.group)}
-		<li class={itemClass}>
-			{item.count}
-			{messages[`enum_passengerKind_${item.group}_name`]?.() ?? item.group}
-			×
-			{formatEuro(item.unitPrice)}
-		</li>
-	{/each}
-</ul>
+<table class="w-[180px] {wrapperClass}">
+	<tbody>
+		{#each items.filter((i) => i.count > 0) as item (item.group)}
+			<tr>
+				<td class="w-full">{messages[`enum_passengerKind_${item.group}_name`]?.() ?? item.group}</td
+				>
+				<td class="text-right tabular-nums">{item.count}</td>
+				<td class="px-1 text-center">×</td>
+				<td class="text-right tabular-nums">{formatEuro(item.unitPrice)}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
