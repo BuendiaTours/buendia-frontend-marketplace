17b2b7e718b7914ae55bd409795ff9678f4a20de
Alejandro Menendez <alejandromenendez@buendia.travel>
2026-04-24 08:41:17 +0200

    feat: remove the roboto font and change the trustpilot logo to svg

--- src/lib/components/marketplace/TrustpilotRating.svelte
+++ src/lib/components/marketplace/TrustpilotRating.svelte
@@ -13,6 +13,5 @@
 	class="flex items-center gap-1.5"
 >
 	<span class="p-base text-neutral-200">{score.toFixed(1)} sobre 5</span>
-	<img src="/marketplace/footer/trust_star.svg" class="-mt-1" alt="Trustpilot" loading="lazy" />
-	<span class="p-trustpilot text-white">Trustpilot</span>
+	<img src="/marketplace/footer/trust.svg" class="-mt-1" alt="Trustpilot" loading="lazy" />
 </a>
